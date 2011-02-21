17a3cd4b4a9e22bad13ad3a2f59d77449d5419d9
David Heward <dave@bytewire.co.uk>
2011-02-21 17:13:21 +0000

    Some problems with the index.php include path on a remote server

--- index.php
+++ index.php
@@ -41,8 +41,8 @@
 		error_reporting(E_ALL);
 		ini_set('display_errors','On');
 		//ini_set('display_errors','Off');
-		ini_set('log_errors', 'On');
-		ini_set('error_log', ROOT.DS.'tmp'.DS.'logs'.DS.'error.log');
+		//ini_set('log_errors', 'On');
+/* 		/ini_set('error_log', ROOT.DS.'tmp'.DS.'logs'.DS.'error.log'); */
 	}		
 	
 	# Require the bootstrap file
