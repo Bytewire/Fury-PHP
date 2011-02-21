fd9631251c7af4adf0601d549378b02a2d846a1b
David Heward <dave@bytewire.co.uk>
2011-02-21 15:32:52 +0000

    pushing some options for local and collaboritive editing

--- index.php
+++ index.php
@@ -21,7 +21,8 @@
 		ini_set('display_errors','On');
 	} else {
 		error_reporting(E_ALL);
-		ini_set('display_errors','Off');
+		ini_set('display_errors','On');
+		//ini_set('display_errors','Off');
 		ini_set('log_errors', 'On');
 		ini_set('error_log', ROOT.DS.'tmp'.DS.'logs'.DS.'error.log');
 	}		
