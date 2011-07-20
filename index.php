174943fd674ef023f8ba2c67d8ef0e500ec13254
Dave Heward <dave@bytewire.co.uk>
2011-07-20 14:47:11 +0100

    some changes to the live

--- index.php
+++ index.php
@@ -1,59 +1,91 @@
 <?php
 	
+/**
+*
+*  Welcome to FURY, a PHP, in Development, Game Framework.
+*/	
+
+
 	// =========== 
-	// ! Version   
+	// ! FURY Version   
 	// =========== 
 	
 	define('FURY_VERSION',	'1.0.0');	
-	
-	echo $_SERVER['HTTP_HOST'];
-	
-	echo '<br><Br>';
-	
-	print_r($_SERVER);
+
 	
 	// =========== 
 	// ! Development environment globals   
 	// ===========
 	
-	// Decide whether in development mode or not.
-	
-	// =========== 
-	// ! Use the test variables to decide on config settings.
-	// Create better more seamless approach to local and remote coding. 
-	// Could also be changed to be anything you like but localhost works well for us here.  
-	// =========== 
+	/**
+	*
+	* Development environment flags
+	* Use the test variables to decide on config settings.
+	* Create better more seamless approach to local and remote coding. 
+	* Could also be changed to be anything you like but localhost works well for us here.  
+	*
+	*
+	*	1. Live
+	* 	2. Stage
+	*	3. Local
+	*/			
 			
 	# Set some defines and call our bootstrap
 	if($_SERVER['HTTP_HOST']){
-    switch($_SERVER['HTTP_HOST']){
-        case "localhost:8888": 
-            define('DEVELOPMENT_ENVIRONMENT' , true);
-            define('EXT', '.php');
-            define('DS', DIRECTORY_SEPARATOR);
-            define('ROOT', dirname(dirname(__FILE__)).'/boardwalk/');
-            define('SYS', 'core/');
-            define('APPPATH','application');
-            break;
-        default: 
-            define('DEVELOPMENT_ENVIRONMENT' , false);
-            define('EXT', '.php');
-            define('DS', DIRECTORY_SEPARATOR);
-            define('ROOT', dirname(dirname(__FILE__)).'/http/');
-            define('SYS', 'core/');
-            define('APPPATH','application');
-    }	
+	
+	    switch($_SERVER['HTTP_HOST']){
+	    	
+	    	/* Local */
+	    	
+	        case "localhost:8888": 
+	            define('DEVELOPMENT_ENVIRONMENT' , 3);
+	            define('EXT', '.php');
+	            define('DS', DIRECTORY_SEPARATOR);
+	            define('ROOT', dirname(dirname(__FILE__)).'/boardwalk/');
+	            define('SYS', 'core/');
+	            define('APPPATH','application');
+	            break;
+	          
+	        /* Stage */  
+	            
+	        case "87.124.85.10":
+	            define('DEVELOPMENT_ENVIRONMENT' , 2);
+	            define('EXT', '.php');
+	            define('DS', DIRECTORY_SEPARATOR);
+	            define('ROOT', dirname(dirname(__FILE__)).'/http/');
+	            define('SYS', 'core/');
+	            define('APPPATH','application');        	
+	        	break;
+	       	
+	       	/* Live */ 	
+	        	
+	        default: 
+	            define('DEVELOPMENT_ENVIRONMENT' , 1);
+	            define('EXT', '.php');
+	            define('DS', DIRECTORY_SEPARATOR);
+	            define('ROOT', dirname(dirname(__FILE__)).'/http/');
+	            define('SYS', 'core/');
+	            define('APPPATH','application');
+	     		break;
+	    }
+	    	
 	}else{
+	
+		/* If no environment was defined, use this as a default */
+	
         define('DEVELOPMENT_ENVIRONMENT' , true);
         define('EXT', '.php');
         define('DS', DIRECTORY_SEPARATOR);
         define('ROOT', dirname(dirname(__FILE__)).'/boardwalk/');
         define('SYS', 'core/');	
         define('APPPATH','application');
+        
 	}
+	
+	
 	# True error reporting should be set here, to find problems right from the route.	
 	
-	if (DEVELOPMENT_ENVIRONMENT == TRUE) {
+	if (DEVELOPMENT_ENVIRONMENT === TRUE) {
 		error_reporting(E_ALL);
 		ini_set('display_errors', 'On');
 	} else {
@@ -66,5 +98,5 @@
 	
 	# Require the bootstrap file		 
 	
-	//require_once (ROOT . 'core' .DS. 'bootstrap.php');
+	require_once (ROOT . 'core' .DS. 'bootstrap.php');
 	
